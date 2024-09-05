a935f8a54f0483fdb4acf2c98946af5538895a7f
Taha Kaplan <66365263+kaplanseunt@users.noreply.github.com>
2024-09-05 13:19:56 +0300

    Update and rename e-twosums.py to easy.py

--- neet/easy.py
+++ neet/easy.py
@@ -32,3 +32,21 @@ class SolutionValidPandrome:
                 newStr +=c.lower()
         return newStr == newStr[::-1]
 
+####
+
+class ValidateParanthesis:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = { ")":"(","]":"[","}":"{"}
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1]==closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False 
+            else:
+                stack.append(c)
+        return True if not stack else False    
+
+###
